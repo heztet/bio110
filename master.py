c56ee3bbbe58fe889e1d1796894e02ea2f6e5246
Nicky Marino <nsmarino96@gmail.com>
2017-03-03 01:43:27 -0500

    Add Mito class

--- master.py
+++ master.py
@@ -1,23 +1,48 @@
 from graphics import *
 import random
 
+random.seed()
+windowDimX = 500
+windowDimY = 200
+distFromEdge = 50
 
 def main():
     # Create window
-    windowDimX = 500
-    windowDimY = 200
     win = GraphWin("Neuron Mitochondria", windowDimX, windowDimY, autoflush=False)
 
     # Create neuron body
-    distFromEdge = 50
-    neuronLength = windowDimX - distFromEdge * 2
-    neuronHeight = windowDimY - distFromEdge * 2
     neuronBody = Rectangle(Point(0 + distFromEdge, 0 + distFromEdge), Point(windowDimX - distFromEdge, windowDimY - distFromEdge))
     neuronBody.draw(win)
 
     # Create mitochondria objects
-    mito = []
-    numMito = 15
+    mitos = []
+    mitosNum = 10
+
+    for i in range(0, mitosNum):
+        # Randomly choose right (1) or left (0) side
+        mitos.append(Mito(win))
+
+    # Run until mouse is clicked
+    while (not win.checkMouse()):
+        for m in mitos:
+            # Randomly choose whether to draw new mito
+            if (not m.drawn):
+                m.randDraw(10000)
+            # Move mito if drawn
+            else:
+                m.move()
+            # Reset mito if it's crossed the neuron body
+            if (m.checkEnd()):
+                mitos.remove(m)
+                mitos.append(Mito(win))
+        # Refresh the window
+        update()
+        print(len(mitos))
+    # Pause for click in window
+    win.getMouse()
+    win.close()
+
+class Mito(object):
     heightBuffer = 10
     minHeight = distFromEdge + heightBuffer
     maxHeight = windowDimY - distFromEdge - heightBuffer
@@ -25,48 +50,48 @@ def main():
     rightSide = windowDimX - distFromEdge
     mitoWidth = 50
     mitoHeight = 10
-    for i in range(0, numMito):
-        # Randomly choose right (1) or left (0) side
-        side = random.randrange(0, 2)
-        x = (leftSide, rightSide)[side]
-        y = random.uniform(minHeight, maxHeight)
-        p1 = Point(x - mitoWidth / 2, y - mitoHeight / 2)
-        p2 = Point(x + mitoWidth / 2, y + mitoHeight / 2)
-        drawn = False
-        oval = 0
-        mito.append([side, p1, p2, drawn, oval])
+    dx = windowDimX / 50000
 
-    # Run until mouse is clicked
-    while (not win.checkMouse()):
-        for i, m in enumerate(mito):
-            # Randomly choose whether to draw new mito
-            if ((not mito[i][3]) and (random.randrange(0, 10000) == 0)):
-                mito[i][4] = Oval(mito[i][1], mito[i][2])
-                mito[i][4].draw(win)
-                mito[i][3] = True
-            # Move mito if drawn
-            if (mito[i][3]):
-                dist = windowDimX / 50000
-                # Reverse direction if moving left
-                if (mito[i][0] == 1):
-                    dist *= -1
-                mito[i][4].move(dist, 0)
-                mito[i][1] = mito[i][4].getP1()
-                mito[i][2] = mito[i][4].getP2()
-            # Reset mito if it's crossed the neuron body
-            midPointX = Line(mito[i][1], mito[i][2]).getCenter().getX()
-            if not (leftSide <= midPointX <= rightSide):
-                mito[i][4].undraw()
-                mito[i][3] = False
+    def __init__(self, window):
+        self.onRight = (False, True)[random.randrange(0, 2)] # 1 = right, 0 = left
+        if self.onRight:
+            x = Mito.rightSide
+        else:
+            x = Mito.leftSide
+        y = random.uniform(Mito.minHeight, Mito.maxHeight)
+        self.p1 = Point(x - Mito.mitoWidth / 2, y - Mito.mitoHeight / 2)
+        self.p2 = Point(x + Mito.mitoWidth / 2, y + Mito.mitoHeight / 2)
+        self.mid = Line(self.p1, self.p2).getCenter()
+        self.drawn = False
+        self.oval = None
+        self.window = window
 
-        # Refresh the window
-        update()
+    def randDraw(self, chance):
+        if (random.randrange(0, 10000) == 0):
+            self.draw()
 
-def PointSubtract(p1, p2):
-    return Point(p2.getX() - p1.getX(), p2.getY() - p1.getY())
+    def draw(self):
+        self.oval = Oval(self.p1, self.p2)
+        self.oval.draw(self.window)
+        self.drawn = True
 
-    # Pause for click in window
-    win.getMouse()
-    win.close()
+    def move(self):
+        dist = Mito.dx
+        if (self.onRight):
+            dist *= -1
+        self.oval.move(dist, 0)
+        self.p1 = self.oval.getP1()
+        self.p2 = self.oval.getP2()
+        self.mid = Line(self.p1, self.p2).getCenter()
+
+    def checkEnd(self):
+        if not (Mito.leftSide <= self.mid.getX() <= Mito.rightSide):
+            self.undraw()
+            return True
+        else:
+            return False
+
+    def undraw(self):
+        self.oval.undraw()
 
 main()
